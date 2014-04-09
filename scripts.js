33df170f8c03a9a77549c43979ca686d42e9a39d
Joel Edwards <joeledwards@gmail.com>
2014-04-09 01:07:51 -0600

    Got basic broadcast messaging working through Redis.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
-$(document).ready(function ()
+$(document).ready(function()
 {
-    console.log("jQuery is ready");
+	console.log("jQuery is ready");
 
-    var socket = io.connect('http://localhost');
+	var socket = io.connect('http://localhost:8888/');
 
-    socket.on('message', function (data)
-    {
-        console.log("Received message: ", data);
-    });
+	socket.on('message', function(data)
+	{
+		console.log("Received message: ", data);
+		
+		$('#display').val($('#display').val() + "\n" + Base64.decode(data.body));
+	});
+
+	$('#msg').keypress(function(e)
+	{
+		if (e.which == 13)
+		{
+			var message = $('#msg').val();
+			$('#msg').val('');
+			console.log("message: " + message);
+
+			socket.emit('broadcast', {
+				encoding : "base64",
+				body : Base64.encode(message)
+			});
+
+			return false;
+		}
+	});
 });
--- server.js
+++ server.js
@@ -8,70 +8,74 @@ var broadcast_channel = "messages";
 
 function makeRedisClient()
 {
-    var client = redis.createClient(redis_port, redis_host);
-    client.auth(redis_auth, function(result)
-    {
-        console.log("created new redis client.");
-    });
+	var client = redis.createClient(redis_port, redis_host);
+	client.auth(redis_auth, function(result)
+	{
+		console.log("created new redis client.");
+	});
 
-    return client;
+	return client;
 }
 
 var g_red = makeRedisClient();
 
 console.log("Resources have been loaded.", b64.encode('Joel Edwards'));
 
-io.sockets.on('connection', function (socket)
+io.sockets.on('connection', function(socket)
 {
-    console.log("connection established");
+	console.log("connection established");
 
-    var client_id = g_red.incr('last-client-id');
-    var red = makeRedisClient();
-    var direct_channel = 'client-' + client_id;
+	g_red.incr('last-client-id', function(error, buffer) {
+		var client_id = buffer;
+		var red = makeRedisClient();
+		var direct_channel = 'client-' + client_id;
 
-    red.on('psubscribe', function(pattern, count)
-    {
-        console.log("client " + client_id + " subscribed to channel(s) matching \"" + pattern
-            + "\" (" + count + " total subscriptions)");
-    });
+		console.log("client id: " + buffer);
 
-    red.on('pmessage', function(pattern, channel, message)
-    {
-        console.log("Message from " + channel + ": " + message);
-        socket.emit('message', {
-            encoding: "base64",
-            body: b64.encode(message)
-        });
-    });
+		red.on('psubscribe', function(pattern, count) {
+			console.log("client " + client_id
+					+ " subscribed to channel(s) matching \"" + pattern
+					+ "\" (" + count + " total subscriptions)");
+		});
 
-    red.on('punsubscribe', function(pattern, count)
-    {
-        console.log("client " + client_id + " un-subscribed to channel(s) matching \"" + pattern + "\" (" + count + " total subscriptions)");
-    });
+		red.on('pmessage', function(pattern, channel, message) {
+			var decoded = b64.decode(message);
+			console.log("Message from " + channel + ": " + decoded);
+			socket.emit('message', {
+				encoding : "base64",
+				body : message
+			});
+		});
 
-    red.psubscribe(broadcast_channel, direct_channel);
+		red.on('punsubscribe', function(pattern, count) {
+			console.log("client " + client_id
+					+ " un-subscribed to channel(s) matching \"" + pattern
+					+ "\" (" + count + " total subscriptions)");
+		});
 
-    socket.on('broadcast', function (data)
-    {
-        var message = (data.encoding === "base64") ? b64.decode(data.body) : data.body;
-        console.log("Broadcast message: " + message);
-        g_red.publish(broadcast_channel, b64.encode(data));
-    });
+		red.psubscribe(broadcast_channel, direct_channel);
 
-    socket.on('direct', function (data)
-    {
-        var recipient = data.recipient;
-        var message = (data.encoding === "base64") ? b64.decode(data.body) : data.body;
-        console.log("Direct message to client " + recipient + ": " + message);
-        g_red.publish('client-' + recipient, b64.encode(data));
-    });
+		g_red.publish(direct_channel, "Welcome client " + client_id);
+	});
 
-    socket.on('disconnect', function()
-    {
-        console.log("client " + client_id + " disconnected");
-        red.punsubscribe();
-        red.end();
-    });
+	socket.on('broadcast', function(data) {
+		var message = (data.encoding === "base64") ? b64.decode(data.body)
+				: data.body;
+		console.log("Broadcast message: " + message);
+		g_red.publish(broadcast_channel, b64.encode(message));
+	});
 
-    //g_red.publish(direct_channel, "Welcome client " + client_id);
+	socket.on('direct', function(data) {
+		var recipient = data.recipient;
+		var message = (data.encoding === "base64") ? b64.decode(data.body)
+				: data.body;
+		console.log("Direct message to client " + recipient + ": " + message);
+		g_red.publish('client-' + recipient, b64.encode(message));
+	});
+
+	socket.on('disconnect', function() {
+		console.log("client " + client_id + " disconnected");
+		red.punsubscribe();
+		red.end();
+	});
 });
