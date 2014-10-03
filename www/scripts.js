65ba0c98c1e7ac773a5f6ad32c3afcc74e6737f0
Joel Edwards <joeledwards@gmail.com>
2014-10-03 09:56:57 -0600

    Increased console logging to determine what is going on with component sizing for auto-scrolling support Reformatted scripts.js to use spaces instead of tabs for indentation Added lodash

--- www/scripts.js
+++ www/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function()
 {
-	console.log("jQuery is ready");
+    console.log("jQuery is ready");
 
-	var socket = io.connect('/');
+    var socket = io.connect('/');
     var client_id;
 
     socket.on('welcome', function(data)
@@ -12,36 +12,41 @@ $(document).ready(function()
         $('div.client span').append('' + data.client_id);
     });
 
-	socket.on('message', function(data)
-	{
-		console.log("Received message: ", data);
+    socket.on('message', function(data)
+    {
+        console.log("Received message: ", data);
 
         var classify = data.from == client_id ? "sent" : "received";
         var who = data.from == client_id ? "me" : data.from;
 
-		$('#display').append('<div class="message ' + classify + '"><span class="id">'
-            + who +': </span>'
-            + Base64.decode(data.body) + '</div>');
+        $('#display').append('<div class="message ' + classify + '"><span class="id">'
+            + who +': </span>' + Base64.decode(data.body) + '</div>');
 
-        $("body").scrollTop($(document).height());
-	});
+        console.log("    window:", $(window).height());
+        console.log("  document:", $(document).height());
+        console.log("   display:", $("#display").height());
+        console.log("      body:", $("body").height());
+        console.log("      html:", $("html").height());
 
-	$('#msg').keypress(function(e)
-	{
-		if (e.which == 13)
-		{
-			var message = $('#msg').val();
-			$('#msg').val('');
-			console.log("message: " + message);
+        $('#display').scrollTop($('#display').height() - $('body').height());
+    });
 
-			socket.emit('broadcast', {
-				encoding : "base64",
-				body : Base64.encode(message)
-			});
+    $('#msg').keypress(function(e)
+    {
+        if (e.which == 13)
+        {
+            var message = $('#msg').val();
+            $('#msg').val('');
+            console.log("message: " + message);
 
-			return false;
-		}
-	});
+            socket.emit('broadcast', {
+                encoding : "base64",
+                body : Base64.encode(message)
+            });
+
+            return false;
+        }
+    });
 
     $('#msg').focus();
 });
