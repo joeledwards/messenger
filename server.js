51781a5a24593e7603431f5cb200b73e7305ec5c
Joel Edwards <joeledwards@gmail.com>
2014-10-09 00:29:27 -0600

    Added support for client aliases
    server.js
    - added support for unique aliases tied to client IDs
    
    www/index.html
    - added alias input
    
    www/scripts.js
    - added client-side alias support
    
    www/styles.css
    - added styling for server messages

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+/*
+TODO: FEATURES
+- persist their user's alias via a cookie
+- allow users to send private messages to another user
+  (this should be unified with the main chat window, but identfied as private)
+  (private messaging should be opt-in)
+- add simple account support (username and passphrase)
+- allow users to set up private group sessions
+  (only invited users can join a group session)
+- add the ability to filter by
+  - message content
+  - message source (broadcast, client, group)
+- add ability to clear all or historical messages before a certain age
+- add a mechanism for exporting a log? perhaps private messages only?
+
+TODO: TESTING
+- build a test system which verifies session behavior acts as expected
+  (should be a multi-client system that can message itself, and each site knows
+   what to expect during correct operation)
+*/
+
 var node_static = require('node-static');
 var parse_args = require('minimist');
 var io = require('socket.io');
@@ -26,6 +47,7 @@ console.log("Redis auth:", redis_auth);
 function makeRedisClient()
 {
 	var client = redis.createClient(redis_port, redis_host);
+
 	client.auth(redis_auth, function(result)
 	{
 		console.log("created new redis client.");
@@ -53,68 +75,163 @@ io.listen(server).sockets.on('connection', function(socket)
 
     var client_key;
     var client_id;
+    var client_alias;
     var red;
     var direct_chanel;
 
-	g_red.incr('last-client-id', function(error, buffer) {
+	g_red.incr('last-client-id', function(error, buffer)
+    {
 		client_id = buffer;
+        client_alias = b64.encode("" + client_id);
         client_key = key_client_map_prefix + client_id;
 		red = makeRedisClient();
 		direct_channel = 'client-' + client_id;
 
-        socket.emit('welcome', {
-            client_id : client_id
-        });
+        socket.emit('welcome', { client_id : client_id });
 
 		console.log("client id: " + buffer);
 
-		red.on('psubscribe', function(pattern, count) {
+        // Log all channel subscribe actions
+		red.on('psubscribe', function(pattern, count)
+        {
 			console.log("client " + client_id
 					+ " subscribed to channel(s) matching \"" + pattern
 					+ "\" (" + count + " total subscriptions)");
 		});
 
-		red.on('pmessage', function(pattern, channel, message) {
+        // Forward all pub/sub channel messages to the socket
+		red.on('pmessage', function(pattern, channel, message)
+        {
 			var parsed = JSON.parse(message);
 			console.log("Message from " + channel + ": " + parsed);
 			socket.emit('message', parsed);
 		});
 
-		red.on('punsubscribe', function(pattern, count) {
+        // Log all channel un-subscribe actions
+		red.on('punsubscribe', function(pattern, count)
+        {
 			console.log("client " + client_id
 					+ " un-subscribed to channel(s) matching \"" + pattern
 					+ "\" (" + count + " total subscriptions)");
 		});
 
+        // Subscribe to pub/sub channels
 		red.psubscribe(broadcast_channel, direct_channel);
 
-		g_red.publish(direct_channel, "Welcome client " + client_id);
+        // Send welcome message to the client. Cannot send through Redis as it may not
+        // yet be fully set up.
+        socket.emit('message', {
+            from : 0,
+            alias : b64.encode("server"),
+            body : b64.encode("Welcome client " + client_id)
+        });
 	});
 
-	socket.on('broadcast', function(data) {
-		var message = (data.encoding === "base64") ? b64.decode(data.body)
-				: data.body;
-        var encodedMessage = b64.encode(message);
-		console.log("Broadcast message: " + message);
-		g_red.publish(broadcast_channel, JSON.stringify({
+	socket.on('broadcast', function(data)
+    {
+		console.log("Broadcast message: " + b64.decode(data.body));
+
+        g_red.publish(broadcast_channel, JSON.stringify({
             from : client_id,
-            encoding : "base64",
-            body : encodedMessage
+            alias : client_alias,
+            body : data.body
         }));
 	});
 
-	socket.on('direct', function(data) {
-		var recipient = data.recipient;
-		var message = (data.encoding === "base64") ? b64.decode(data.body)
-				: data.body;
-		console.log("Direct message to client " + recipient + ": " + message);
-		g_red.publish('client-' + recipient, b64.encode(message));
+    // Handshake hash should have mappings of source clients to destination clients.
+    // The one initiating the "handshake" gets automatically added (initiator, invitee)
+    // When the invitee acttpts the reverse gets added (invitee, initiator)
+    // There should be a way to throttle the number of invites (perhaps once every minute?
+	socket.on('direct', function(data)
+    {
+		console.log("Direct message to client " + data.recipient + ": "
+            + b64.decode(data.body));
+
+		g_red.publish('client-' + data.recipient, JSON.stringify({
+            from : client_id,
+            alias : client_alias,
+            body : data.body
+        }));
 	});
 
-	socket.on('disconnect', function() {
+    socket.on('alias', function(data)
+    {
+        console.log("client " + client_id + " alias request: " + data);
+        var new_alias = data.alias;
+
+        // Attempt to set the alias of the client
+        g_red.hsetnx("client-alias-hash", new_alias, client_id, function(err, result)
+        {
+            g_red.hget("client-id-hash", client_id, function(err, old_alias)
+            {
+                if (!err && old_alias)
+                {
+                    g_red.hdel("client-alias-hash", old_alias, function(err, result)
+                    {
+                        if (!err && result)
+                        {
+                            console.log("client " + client_id + " has been disassociated"
+                                + " from alias '" + b64.decode(old_alias) + "'");
+                        }
+                    });
+                }
+            });
+
+            g_red.hget("client-alias-hash", new_alias, function(err, alias_owner)
+            {
+                if (!err && client_id == alias_owner)
+                {
+                    console.log("client " + client_id + " has been associated with alias '"
+                        + b64.decode(new_alias) + "'");
+
+                    // If the alias was set, store the alias in the reverse hash
+                    var old_alias = client_alias;
+                    client_alias = new_alias;
+                    g_red.hset("client-id-hash", client_id, client_alias);
+                    socket.emit('alias', { alias : client_alias });
+
+                    g_red.publish(broadcast_channel, JSON.stringify({
+                        from : 0,
+                        alias : b64.encode("server"),
+                        body : b64.encode("Client " + client_id + " has changed alias from '"
+                            + b64.decode(old_alias) + "' to '" + b64.decode(new_alias) + "'")
+                    }));
+                }
+                else
+                {
+                    console.log("client " + client_id + " requested alias '"
+                        + b64.decode(new_alias)
+                        + "', but it was already in use by client " + alias_owner);
+
+                    // If the alias could not be set, inform the requesting client
+                    socket.emit('message', {
+                        from : 0,
+                        alias : b64.encode("server"),
+                        body : b64.encode("Alias '" + b64.decode(new_alias)
+                            + "' is already in use")
+                    });
+                }
+            });
+        });
+    });
+
+	socket.on('disconnect', function()
+    {
 		console.log("client " + client_id + " disconnected");
 		red.punsubscribe();
 		red.end();
+
+        g_red.hget("client-alias-hash", client_id, function(err, alias)
+        {
+            if (!err)
+            {
+                console.log("dissassociating client " + client_id + " from alias '"
+                    + alias + "'");
+
+                g_red.hdel("client-alias-hash", alias);
+                g_red.hdel("client-alias-hash", client_id);
+            }
+        });
 	});
 });
 
--- www/scripts.js
+++ www/scripts.js
@@ -4,20 +4,36 @@ $(document).ready(function()
 
     var socket = io.connect('/');
     var client_id;
+    var client_alias;
 
     socket.on('welcome', function(data)
     {
         client_id = data.client_id;
-        console.log("I am client " + data.client_id);
-        $('div.client span').append('' + data.client_id);
+        client_alias = "" + client_id;
+        console.log("I am client " + client_id);
+        $('div.client span').append("" + client_id);
+    });
+
+    socket.on('alias', function(data)
+    {
+        console.log("New alias received: ", data);
+
+        client_alias = Base64.decode(data.alias);
+        console.log("I am '" + client_alias + "' (client " + client_id + ")");
+        $('div.client span').val(client_alias +" &nbsp; (" + client_id + ")");
+        
+        $('#alias').val(client_alias);
+        $('#alias').addClass('valid-alias');
+        $('#alias').removeClass('invalid-alias');
     });
 
     socket.on('message', function(data)
     {
         console.log("Received message: ", data);
 
-        var classify = data.from == client_id ? "sent" : "received";
-        var who = data.from == client_id ? "me" : data.from;
+        var classify = data.from == client_id ? "sent" :
+            (data.from == 0 ? "server" : "received");
+        var who = data.from == client_id ? "me" : Base64.decode(data.alias);
 
         $('#display').append('<div class="message ' + classify + '"><span class="id">'
             + who +': </span>' + Base64.decode(data.body) + '</div>');
@@ -32,6 +48,33 @@ $(document).ready(function()
         $('#main').scrollTop($('#main').height() - $('body').height());
     });
 
+    $('#alias').keypress(function(e)
+    {
+        if ($('#alias').val == client_alias)
+        {
+            $('#alias').addClass('valid-alias');
+            $('#alias').removeClass('invalid-alias');
+        }
+        else
+        {
+            $('#alias').addClass('invalid-alias');
+            $('#alias').removeClass('valid-alias');
+
+            if (e.which == 13)
+            {
+                var new_alias = $('#alias').val();
+
+                if (new_alias)
+                {
+                    console.log("requesting alias '" + new_alias + "'");
+                    socket.emit('alias', {
+                        alias : Base64.encode("" + new_alias)
+                    });
+                }
+            }
+        }
+    });
+
     $('#msg').keypress(function(e)
     {
         if (e.which == 13)
@@ -41,7 +84,6 @@ $(document).ready(function()
             console.log("message: " + message);
 
             socket.emit('broadcast', {
-                encoding : "base64",
                 body : Base64.encode(message)
             });
 
